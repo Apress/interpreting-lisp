556705224c4224350f0b853a24f54e0e881d20fa
Jianbing Tai <jianbing.tai@outlook.com>
2023-03-22 11:39:28 +0800

    Update source code
    1. Add "not included" 2 header files (GNU_C, Turbo C)
    2. Adjust int16/int32 defination position in source code to pass compile
    3. Format lisp.c with code formatting tool
    4. Remove a few compiler mentioned "not used variables"

---
+++ Interpreting Lisp source code/turbcenv.h
@@ -0,0 +1,13 @@
+#ifndef LISP_TURBOCENV_HEADER
+#define LISP_TURBOCENV_HEADER
+
+#define int16_t short
+#define int32_t long
+
+#include <math.h>
+#include <setjmp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#endif
